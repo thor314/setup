3fe7f8963e0dfab6240e90f468b1db9103662c7f
Thor <thorck@protonmail.com>
2022-04-15 16:55:15 +0200

    apt organized

--- apt.sh
+++ apt.sh
@@ -1,126 +1,98 @@
 #!/bin/bash
-
+alias ins='apt install -y'
+alias cins='cargo install'
+alias sins='snap install'
+alias pins='pip3 install'
 apt update
+ins git # version control, if it's not already here
 
-# zsh
-apt -y install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# open pdfs w evince
-apt install evince
-
-# i3 tiling window manager
-# rofi as dmenu replacement
-# scrot for screenshots
-# feh for wallpaper
-apt -y install i3 rofi feh conky scrot
-# dunst for notifications
-# conky to display information about system
-# NetworkManager-gnome to manage network conections
-
-# discord
-pushd ~/Downloads
-wget -O discord-0.0.1.deb https://discordapp.com/api/download?platform=linux&format=deb
-apt -y install discord
+########################
+### PACKAGE MANAGERS ###
+########################
+# Rust, Python, Node
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+rustup update
+rustup toolchain install nightly
+rustup default nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+git clone https://github.com/rust-analyzer/rust-analyzer.git
+pushd rust-analyzer
+cargo xtask install --server
 popd
 
-# spotify https://www.spotify.com/us/download/linux/
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-apt -y install spotify-client
+ins python3-pip
 
-# apt -y install apt-transport-https curl gnupg
-apt -y install emacs sqlite3 ripgrep
-
-# rust tools
-apt -y install llvm clang
-
-# npm, node via nvm
 # https://github.com/nvm-sh/nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node
 
-# Near cli
-npm install -g near-cli
+################
+### PACKAGES ###
+################
 
-# brave browser
-#curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-#echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-#apt -y install brave-browser
+# fully featured shell
+ins zsh # better shell than bash
+## zsh configuration framework
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ins vim-gui-common # fully featured vim
+ins vim-runtime # fully featured vim
 
-# Gnome
-apt -y install gnome-tweaks
+# CLI tools
+cins ripgrep # better grep
+ins gist # quickly share files via github
+ins hugo # static site generator blog management
+ins hub # github
+pip3 install tldr # man-lite; quick, what does this CLI tool do?
+ins pass # password manager CLI
+ins xclip # clipboard
+ins scrot # screenshot
+pins pz # Python as a command line tool
+cins --locked bat # better than cat
+git clone https://github.com/eth-p/bat-extras # bat(diff|grep|man|pipe|watch)
+pushd bat-extras && ./build.sh --install && popd
 
-# Messenging apps
-# https://itsfoss.com/install-signal-ubuntu/
-wget -O- https://updates.signal.org/desktop/apt/keys.asc |  sudo apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" |  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-apt -y install signal-desktop
+## if I eventually like window-managers like i3 or leftwm again, use these
+# ins rofi # window switcher, app launcher, dmenu replacement
+# ins feh # set background image
+# ins conky # lightweight system monitor, display info
+# ins dunst # desktop notifications
 
-add-apt-repository ppa:atareao/telegram
-apt -y install telegram
+# Programming dependencies
+ins llvm clang 
 
-# apt -y trails latest git version by a couple years
-#add-apt-repository ppa:git-core/ppa
-# apt -y reinstall git
-# apt -y install gist
+# latex 
+ins texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra 
 
-# mail utility
-# apt -y install sendmail
+# GUI applications
+ins gnome-tweaks # tweak destkop settings gui
+ins evince # pdf viewer
+sins firefox # browser
+sins chromium # secondary browser
+sins notion-snap # secondary notes
+sins signal-desktop # messaging
+sins spotify # spotify
 
-# ssl development library
-# apt -y install libssl-dev
+# Python ; idk, I said python dependencies at one point
+# libxcb-xfixes0-dev libxcb-shape0-dev 
+# pins pywal 
 
-# export latex files
-apt -y install texlive-latex-base
-apt -y install texlive-fonts-recommended
-apt -y install texlive-fonts-extra # Fat, 1.6Gb
-apt -y install texlive-latex-extra
+###########################
+# shame, shame, be better #
+###########################
+# wget https://zoom.us/client/latest/zoom_amd64.deb
+# dpkg -i zoom_amd64.deb
+# ins discord 
+# 
+# # install obsidian here: https://obsidian.md/download
+# # sudo snap install --dangerous obsidian_0.13.23_amd64.snap
+# 
+# # vim plug 
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# zoom video conferencing
-wget https://zoom.us/client/latest/zoom_amd64.deb
-dpkg -i zoom_amd64.deb
-apt -y install -f
+# redundant config (should be in a vc'd config file)
+git config --global core.editor "code --wait"
+git config --global merge.ff no
 
-# brother printer
-# info: https://support.brother.com/g/b/downloadhowto.aspx?c=us&lang=en&prod=hll2350dw_us_eu_as&os=127&dlid=dlf006893_000&flang=4&type3=625
-wget https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.2-2.gz
-gunzip linux-brprinter-installer-*.*.*-*.gz
-bash linux-brprinter-installer-*.*.*-*
-# Note: setup printer on wifi network then connect via IP address.
-
-# blog stuff: clipboard command line tool
-apt -y install xclip
-apt -y install hugo
-
-# if apt -y error: Target CNF is configured multiple times
-# wget https://github.com/davidfoerster/aptsources-cleanup/archive/refs/tags/v0.1.7.5.2.tar.gz
-# tar -xvf v0.1*
-# cd aptsources-cleanup-0.1.7.5.2
-# ./aptsources-cleanup
-
-## try very hard to get emojis.
-# sudo apt-add-repository ppa:eosrei/fonts\n
-# sudo apt -y update
-# apt -y install fonts-emojione
-## try2 emoji: use debian direct install.
-#git clone https://github.com/gentakojima/emojione-picker-ubuntu; cd emojione-picker-ubuntu
-#git checkout v0.1
-#./install.sh; cd ..
-snap install chromium
-snap install notion-snap
-apt install python3-pip
-
-# excluded: Terra-station
-apt install wmctrl
-
-# https://www.bitlbee.org/user-guide.html
-sudo apt install bitlbee
-purple-discord
-#https://github.com/EionRobb/purple-discord
-
-# install nyxt
-wget https://github.com/atlas-engineer/nyxt/releases/download/2.2.2/nyxt_2.2.2_amd64.deb
-apt install nyxt*.deb
